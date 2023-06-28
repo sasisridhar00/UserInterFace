ed6591ad4714e168c0ad69fdc0e846e00bc298f0
SasiSridhar <135687086+sasisridhar00@users.noreply.github.com>
2023-06-28 07:14:08 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const path = require('path');
+const userRoutes = require('./server/routes/user');
+const postRoutes = require('./server/routes/post');
+//console.log(process.env.dbURL);
+mongoose.connect(process.env.dbURL)
+  .then(console.log("DB Connected!!"))
+  .catch(error => console.log(error));
+
+app.use(express.json());
+
+app.use(express.static(__dirname + "/client/build"));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/client/build', 'index.html')));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin,X-Requested-Width,Content-Type,Accept,Authorization");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS")
+  next();
+});
+
+app.use('/user', userRoutes);
+app.use('/post', postRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}!`));
