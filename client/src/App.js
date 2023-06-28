5ace5b037a0546a9c50449f11682f1d1d38a3218
SasiSridhar <135687086+sasisridhar00@users.noreply.github.com>
2023-06-28 07:28:55 -0400

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+//import Posts from './components/Posts.js';
+//import About from './components/About.js';
+import Register from './components/Register.js';
+import Navbar from './components/Navbar.js';
+import Login from './components/Login.js';
+
+
+
+function App() {
+  return (
+    <div className="App">
+     
+     <Navbar />
+     <Register />
+     <Login />
+     
+ 
+      
+    
+
+    </div>
+  
+  );
+}
+
+export default App;
