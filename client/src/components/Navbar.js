94919b715792d335c1945d34214f91ce9835fe6a
SasiSridhar <135687086+sasisridhar00@users.noreply.github.com>
2023-07-05 07:15:12 -0400

    fetch Update

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,40 +1,46 @@
+
+import { Outlet, Link } from "react-router-dom";
 const Navbar =() =>{
-    return (
-        <div>
-            <nav className="navbar navbar-expand-lg bg-body-tertiary " >
-  <div className="container-fluid" >
-    <a className="navbar-brand" href="#">Postagram</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Register</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Profile</a>
-        </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-           more
-          </a>
-          <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="#">Likes</a></li>
-            <li><a className="dropdown-item" href="#">Followers</a></li>
-            <li><a className="dropdown-item" href="#">Following</a></li>
+  return(
+  <div>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary ">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">Postagram</Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-Link active" to="/About" href="#">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-Link active" to="/login" href="#">Login</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-Link" to="/register" href="#">Register</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-Link" to="/profile" href="#">Profile</Link>
+            </li>
+            <li className="nav-item dropdown">
+              <Link className="nav-Link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                more
+              </Link>
+              <ul className="dropdown-menu">
+                <li><Link className="dropdown-item" href="#">Likes</Link></li>
+                <li><Link className="dropdown-item" href="#">Followers</Link></li>
+                <li><Link className="dropdown-item" href="#">Following</Link></li>
+              </ul>
+            </li>
           </ul>
-        </li>
-      </ul>
-      
-    </div>
-  </div>
-</nav>
+
         </div>
-        
-    );
-}
+      </div>
+    </nav>
+    <Outlet />
+  </div>
+
+)
+  };
 export default Navbar;
