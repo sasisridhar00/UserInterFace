48420c5fd4afdb620f2e7f610cef283dc62cc179
SasiSridhar <135687086+sasisridhar00@users.noreply.github.com>
2023-07-05 07:13:21 -0400

    fetch Update

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,36 @@
 import './App.css';
-//import Posts from './components/Posts.js';
-//import About from './components/About.js';
-import Register from './components/Register.js';
-import Navbar from './components/Navbar.js';
-import Login from './components/Login.js';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Profile from './components/pages/Profile.js';
+import About from './components/pages/About.js';
+import Register from './components/pages/Register.js';
+import Navbar from './components/pages/Navbar.js';
+import Login from './components/pages/Login.js';
+//import { UserProvider } from './context/userContext';
 
 
 function App() {
   return (
-    <div className="App">
-     
-     <Navbar />
-     <Register />
-     <Login />
-     
- 
-      
+  <div className="App">
     
+        <BrowserRouter>
+          <Routes>
+              <Route  element={ <Navbar />}>
+              <Route index element={ <Login />}/>
+              <Route index element={ <Profile />}/>
+              <Route index element={<About />}/>
+              <Route path="register" element={<Register />}/>
+            
+           
+            </Route>
+          </Routes>
+        </BrowserRouter>
+     
 
     </div>
+      
+     
+
+  
   
   );
 }
