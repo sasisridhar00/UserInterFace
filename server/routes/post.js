d155765792f46b1855fa9a83a4f7244abe3c37aa
SasiSridhar <135687086+sasisridhar00@users.noreply.github.com>
2023-07-05 23:55:39 -0400

    fetch Update post.js

--- server/routes/post.js
+++ server/routes/post.js
@@ -4,9 +4,17 @@ const router = express.Router();
 
 // 2. create all routes to access database
 router
-  .post('/newPost', async (req, res) => {
+.get('/', async (_req, res) => {
+  try {
+    const posts = await Post.getAllPosts();
+    res.send(posts);
+  } catch(err) {
+    res.status(401).send({message: err.message});
+  }
+})
+  .post('/Read', async (req, res) => {
     try {
-        const post = await Post.newPost(req.body.id, req.body.postcontent);
+        const post = await Post.Read(req.body.id, req.body.postcontent);
         res.send({...post });
       } catch(error) {
         res.status(401).send({ message: error.message });
@@ -20,6 +28,14 @@ router
       res.send({...post, postcontent: undefined});
     } catch(error) {
       res.status(401).send({ message: error.message });
+    }
+  })
+  .put('/edit', async (req, res) => {
+    try {
+      let note = await Post.editNotes(req.body);
+      res.send({...this.post, postcontent});
+    } catch(err) {
+      res.status(401).send({message: err.message})
     }
   })
 
