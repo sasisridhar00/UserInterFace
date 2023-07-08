d0d54a7fbcbeefe5a719429bc6c1649713a8dd0f
SasiSridhar <135687086+sasisridhar00@users.noreply.github.com>
2023-07-07 23:02:45 -0400

    Final Project App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,29 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-//import Profile from './components/pages/Profile.js';
-import About from './components/pages/About.js';
-import Register from './components/pages/Register.js';
-import Navbar from './components/pages/Navbar.js';
+import { UserProvider } from './context/userContext';
+//import About from './components/pages/About.js';
 import Login from './components/pages/Login.js';
-//import { UserProvider } from './context/userContext';
+import Register from './components/pages/Register.js';
+import Profile from './components/pages/Profile.js';
+import Navbar from './components/pages/Nav.js';
 
 function App() {
   return (
-  <div className="App">
- 
-        <BrowserRouter>
-          <Routes>
-              <Route  path="/" element={ <Navbar />}>
-              <Route index element={<Login />}/>
-              <Route path ="home" element={ <About />}/>
-
-              <Route path="register" element={<Register />}/>
-            
-           
-            </Route>
-          </Routes>
-        </BrowserRouter>
-     
-
+    <div className="App">
+      <UserProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navbar />} >
+            <Route index />
+          <Route path="/login" element={<Login />}/>
+          <Route path="/register" element={<Register />}/>
+          <Route path="/profile" element={<Profile />}/>
+          </Route>
+        </Routes> 
+      </BrowserRouter>  
+      </UserProvider>   
     </div>
-        
   );
 }
-
 export default App;
-     
 
-  
-  
-  );
-}
-
-export default App;
