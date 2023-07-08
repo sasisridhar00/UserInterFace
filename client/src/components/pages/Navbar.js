997f070163516ec661c1ac3c6a14ac08990449cf
SasiSridhar <135687086+sasisridhar00@users.noreply.github.com>
2023-07-07 22:56:08 -0400

    Final Project Navbar.js

--- client/src/components/pages/Navbar.js
+++ client/src/components/pages/Navbar.js
@@ -21,7 +21,7 @@ const Navbar= () => {
 <div>
 <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
-    <Link className="navbar-brand" to="/">Social Media Application</Link>
+    <Link className="navbar-brand" to="/">Postagram</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarid" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
