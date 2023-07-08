d2775d52f6486b12ffc6903c59ca1ebcaf9a0221
SasiSridhar <135687086+sasisridhar00@users.noreply.github.com>
2023-07-07 23:08:59 -0400

    Final submission

--- server/models/user.js
+++ server/models/user.js
@@ -1,57 +1,61 @@
 const mongoose = require("mongoose");
-const bcrypt = require ('bcryptjs');
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
-    username : { type: String, required: true},
-    email: { type: String, required: true},
-    password : { type: String, required: true}
-
-
+  username: { type: String, unique: true, required: true},
+  email: { type: String, unique: true, required: true},
+  password: { type: String, required: true},
+  followers: [String],
+  following: [String]
 })
-const User = mongoose.model("User",userSchema);
 
-async function register(username,email, password) {
-    const user =  await getUser(username);
-    if(user) throw Error('Username already in use');
-   const salt = await bcrypt.genSalt(10);
-   const hashed = await bcrypt.hash(password, salt);
+// 3. create model of schema
+const User = mongoose.model("User", userSchema);
+
+
+//CREATE a user
+async function register(username, email, password) {
+  const user = await getUser(username);
   
-    const newUser = await User.create({
-      username: username,
-      email:email,
-      password: hashed
-    });
+  if(user) throw Error('Username already in use');
+  const salt = await bcrypt.genSalt(10);
+  const hashed = await bcrypt.hash(password, salt)
+  const newUser = await User.create({
+    username: username,
+    email: email,
+    password: hashed
+  });
+
+  return newUser;
+}
+
+// READ a user
+async function login(username, password) {
+  const user = await getUser(username);
+  if(!user) throw Error('User not found');
+
+  const isMatch = await bcrypt.compare(password, user.password)
+  if(!isMatch) throw Error('Wrong password');
   
-    return newUser._doc;
-  }
-  
-  // READ a user
-  async function login(username, password) {
-    const user = await getUser(username);
-    if(!user) throw Error('User not found');
-    const isMatch = await bcrypt.compare(password, user.password);
-    if(!isMatch) throw Error('Wrong Password');
-    return user._doc;
-  }
-  
-  // UPDATE
-  async function updatePassword(id, password) {
-    const user = await User.updateOne({"_id": id}, {$set: { password: password}});
-    return user;
-  }
-  
-  //DELETE
-  async function deleteUser(id) {
-    await User.deleteOne({"_id": id});
-  };
-  
-  // utility functions
-  async function getUser(username) {
+  return user;
+}
+
+// UPDATE
+async function updatePassword(id, password) {
+  const user = await User.updateOne({"_id": id}, {$set: { password: password}});
+  return user;
+}
+
+//DELETE
+async function deleteUser(id) {
+  await User.deleteOne({"_id": id});
+};
+
+// utility functions
+async function getUser(username) {
     return await User.findOne({ "username": username});
   }
   
-  // 5. export all functions we want to access in route files
-  module.exports = { 
-    register, login, updatePassword, deleteUser 
-  };
+// 5. export all functions we want to access in route files
+module.exports = {register, login, updatePassword, deleteUser, getUser};
   
