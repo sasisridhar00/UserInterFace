040910e98229cc9518648112f94c3ba6747e3b34
SasiSridhar <135687086+sasisridhar00@users.noreply.github.com>
2023-07-07 23:09:47 -0400

    Final Project

--- server/routes/post.js
+++ server/routes/post.js
@@ -1,52 +1,40 @@
-const express = require("express");
-const Post = require('../models/post'); //accesses functions in user model file
+
+  const express = require("express");
+const Post = require('../models/post');
 const router = express.Router();
 
-// 2. create all routes to access database
 router
-.get('/', async (_req, res) => {
-  try {
-    const posts = await Post.getAllPosts();
-    res.send(posts);
-  } catch(err) {
-    res.status(401).send({message: err.message});
-  }
-})
-  .post('/Read', async (req, res) => {
-    try {
-        const post = await Post.Read(req.body.id, req.body.postcontent);
-        res.send({...post });
-      } catch(error) {
-        res.status(401).send({ message: error.message });
-      }
-  })
-
-  
-  .put('/update', async (req, res) => {
-    try {
-      const post = await Post.updatePost(req.body.id, req.body.postcontent);
-      res.send({...post, postcontent: undefined});
-    } catch(error) {
-      res.status(401).send({ message: error.message });
+.post('/create', async (req,res) => {
+    try{
+     const post = await Post.create(req.body.username, req.body.posttext);  
+     res.send({...post});
+    } catch(error){
+      res.status(401).send({message: error.message});
     }
-  })
-  .put('/edit', async (req, res) => {
-    try {
-      let note = await Post.editNotes(req.body);
-      res.send({...this.post, postcontent});
-    } catch(err) {
-      res.status(401).send({message: err.message})
+ })  
+ .post('/fetch', async (req,res) => {
+    try{
+     const post = await Post.fetch(req.body.username);  
+     res.send({...post});
+    } catch(error){
+      res.status(401).send({message: error.message});
     }
-  })
-
-  .delete('/delete', async (req, res) => {
-    try {
-      await Post.deletePost(req.body.id);
-      res.send({ success: "Post deleted" });
-    } catch(error) {
-      res.status(401).send({ message: error.message });
+ })
+.put('/updatePost', async (req,res) => {
+    try{
+     const post = await Post.updatePost(req.body.username, req.body.newText);  
+     res.send({...post, password: undefined});
+    } catch(error){
+      res.status(401).send({message: error.message});
     }
-  })
+ })
+ .delete('/deletePost', async (req,res) => {
+    try{
+     await Post.deletePost(req.body.id);  
+     res.send({ success: "Account Deleted" });
+    } catch(error){
+      res.status(401).send({message: error.message});
+    }
+ })
 
-// 3. export router for use in index.js
-module.exports = router;
+ module.exports = router;
